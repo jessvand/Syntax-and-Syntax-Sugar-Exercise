769c09dfff1b8e4297156c3dbf13985430c2e5a0
jessvand <jmvandbeek@gmail.com>
2024-05-03 11:01:46 -0400

    used inferred typing

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var liftingYears = 4;
+           
+            var response = liftingYears <= 9 ? $"With {liftingYears}? You're still just a baby!" : $"With {liftingYears}? You're just starting to get it!";
+
+            Console.WriteLine(response);
+            
+            
         }
     }
 }
+  
